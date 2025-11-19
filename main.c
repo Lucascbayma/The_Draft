4e0fc17959fb2b2f048ad852c403fcf1973655f4
Eros Amancio Nascimento <ean@cesar.school>
2025-11-19 17:00:22 -0300

    colocando a musica novamente no jogo.

--- main.c
+++ main.c
@@ -479,7 +479,7 @@ int main() {
     Texture2D fundoPreto = LoadTexture("images/fundo_preto.png");
     const int tamanhoFonteTitulo = 30; 
     Font fontTitulo = LoadFontEx("assets/PatrickHandSC-Regular.ttf", tamanhoFonteTitulo, NULL, 0); 
-    Music music = LoadMusicStream("audio/music/the_draft_music.ogg");
+    Music music = LoadMusicStream("audio/music/the_draft_music.mp3");
     music.looping = true;
     float musicVolume = 0.5f; 
     SetMusicVolume(music, musicVolume);
